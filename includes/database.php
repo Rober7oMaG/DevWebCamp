6f9033b7a5a9f48552bbe8d3306f8c3a3664f6ac
Roberto Macias <robertomag7@hotmail.com>
2023-06-23 00:07:59 -0600

    Added database port to connection

--- includes/database.php
+++ includes/database.php
@@ -3,7 +3,8 @@ $db = mysqli_connect(
     $_ENV['DB_HOST'] ?? '',
     $_ENV['DB_USER'] ?? '', 
     $_ENV['DB_PASS'] ?? '', 
-    $_ENV['DB_NAME'] ?? ''
+    $_ENV['DB_NAME'] ?? '',
+    $_ENV['DB_PORT'] ?? ''
 );
 
 if (!$db) {
